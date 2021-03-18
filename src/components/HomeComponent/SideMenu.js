37670029f5c16d569e0d6776c9152d9e2f93bf0a
gabriel_at <gabriel-almeida-97@hotmail.com>
2021-03-18 10:56:07 -0300

    correção layout responsivo sidemenu

--- src/components/HomeComponent/SideMenu.js
+++ src/components/HomeComponent/SideMenu.js
@@ -78,6 +78,9 @@ div{
     text-align: center;
     font-family: bree serif;
     font-size: 25px;
+    @media(max-width: 800px) {
+      width: font-size: 15px;
+    }
     color: #d9d9d9;
     font-weight: bold;
   }
